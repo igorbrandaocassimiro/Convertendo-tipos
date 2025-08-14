c337a6094c8797f945edc41c7ee3a2c478fd3ee8
Igor Brandão <igorbrandaocassimirodaflon@gmail.com>
2025-08-14 13:15:36 -0300

    Primeiro commit do programa

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] arge)
+    {
+        decimal pesoContainer = 18.75m; 
+        int veiculosNecessarios = (int)pesoContainer; 
+
+        Console.WriteLine("Veículos necessários: " + veiculosNecessarios); 
+    }
+}
